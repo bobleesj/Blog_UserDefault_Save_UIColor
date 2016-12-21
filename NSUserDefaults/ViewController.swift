192d6314273e59476b44564f3c11c0775118c2ac
Bob Lee <Bob@Bobs-MacBook-Pro.local>
2016-12-21 23:29:18 +0900

    Bob the Developer Dope

--- NSUserDefaults/ViewController.swift
+++ NSUserDefaults/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  let defaults = UserDefaults.standard
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    getUserDefaultsInt()
+    getUserDefaultDict()
+    getUserDefaultColor()
+
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func getUserDefaultsInt() {
+    defaults.set(20, forKey: "myAge")
+    print(defaults.integer(forKey: "myAge"))
+    defaults.set(21, forKey: "myAge")
+    print(defaults.integer(forKey: "myAge"))
   }
-
-
+  
+  func getUserDefaultDict() {
+    let name = ["Real": "SangJoon Lee"]
+    
+    // First Method
+    defaults.set(name, forKey: "name")
+    if let name = defaults.value(forKey: "name") as? [String: String] {
+      print(name) // Downcast from Any? to [String: String]
+    }
+  
+  
+    
+    // Second Method
+    if let name = defaults.dictionary(forKey: "name") as? [String: String] {
+      print(name) // Downcast from [String : Any]? to [String: String]
+    }
+  }
+  
+  func getUserDefaultColor() {
+    defaults.setColor(color: UIColor.red, forKey: "myColor")
+    let myColor = defaults.colorForKey(key: "myColor")
+  }
+  
 }
-
